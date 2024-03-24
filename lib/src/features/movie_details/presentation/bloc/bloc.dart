a6df3c6d4de29efc87eb5157f4260fba8cfdbfd8
Enrique Chua <enrique.chua29@gmail.com>
2024-03-24 11:22:06 +0800

    cleanup unused

--- lib/src/features/movie_details/presentation/bloc/bloc.dart
+++ lib/src/features/movie_details/presentation/bloc/bloc.dart
@@ -1,4 +1,3 @@
 export 'credits/movie_credits_cubit.dart';
 export 'details/movie_details_cubit.dart';
 export 'movie_gallery/movie_gallery_cubit.dart';
-export 'reviews/reviews_cubit.dart';
--- lib/src/features/movie_details/presentation/bloc/reviews/reviews_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'reviews_state.dart';
-
-class ReviewsCubit extends Cubit<ReviewsState> {
-  ReviewsCubit() : super(ReviewsInitial());
-}
